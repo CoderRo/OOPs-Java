fe42ddf666459157b8ce36f22ca8f653dfb3c584
Rohan <40395507+CoderRo@users.noreply.github.com>
2022-03-22 23:59:06 +0530

    Update MethodOverloading.java

--- src/basic/MethodOverloading.java
+++ src/basic/MethodOverloading.java
@@ -13,10 +13,27 @@ public class MethodOverloading {
 	int sum ( double a , double b) {
 		return (int) (a-b);
 	}
+	
+	// Method overloading cannot be achieved by changing the return type:
+	// i.e 
+	
+	//	int add( int a , int b ) {
+	//		return a+b;
+	//	}
+	//	double add ( int a , int b) {
+	//		return a+b;
+	//	}
+	
 	public static void main(String[] args) {
 		MethodOverloading MethObj = new MethodOverloading();
+		
 		System.out.println("Sum methods with 2 arguments " + MethObj.sum(2,3));
 		System.out.println("Sum methods with 3 arguments " + MethObj.sum(2,3,4));
+		
 		System.out.println("Sum methods with 2 arguments " + MethObj.sum(12.9, 11.9));
+		
+//		System.out.println(MethObj.add(1,2));
+//		System.out.println(MethObj.add(3,4));
+		
 	}
 }
